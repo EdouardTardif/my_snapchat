ce1af61c386aeb17fd2d94a298ae63fd01d005cb
unknown <edouardnoel.tardif@gmail.com>
2020-05-25 20:26:53 +0200

    test

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View,  TouchableOpacity,Button } from 'react-native';
+import logo from './assets/snapchat.png';
+import * as ImagePicker from 'expo-image-picker';
+
+
+
 
 export default function App() {
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} /> 
+      <Text  style={styles.instructions}> 
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Choisir une photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +37,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 30,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
